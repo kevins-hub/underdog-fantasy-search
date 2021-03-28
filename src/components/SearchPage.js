437a82793588e44ca4372aaee8f9498376ed6017
kevins-hub <kevinliu428@gmail.com>
2021-03-28 00:00:41 -0600

    First commit

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -54,21 +54,6 @@ class SearchPage extends React.Component {
             playerList: pList
         });
         
-
-        //console.log(pList);
-        /*
-         axios.get(`https://jsonplaceholder.typicode.com/users`)
-         .then(res => {
-           const persons = res.data;
-           this.setState({ persons });
-         })
-
-         axios.get('/api', {
-            params: {
-              foo: 'bar'
-            }
-          });
-        */
     };
 
     render() {
